984a064ca73c207899676314d3687a61e0da1782
Piotr Bielski <bpiotrrr@gmail.com>
2024-11-25 21:55:51 +0100

    Added configs

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,36 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+-- line numbers
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+-- turn off swapfile
+opt.swapfile = false
+
